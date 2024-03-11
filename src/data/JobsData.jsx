e62f244a034399e4411c1acccf8a9e4b7876d6ba
Sampath kumar <jettysampath8@gmail.com>
2024-03-11 10:29:06 +0530

    Update JobsData.jsx

--- src/data/JobsData.jsx
+++ src/data/JobsData.jsx
@@ -1,7 +1,28 @@
 export const jobs = [{
     "id": 1,
-    "location": "Hyderabad",
-    "role": "Senior Consultant",
-    "experience": "4+ years",
-    "salary": "Rs. 100000- Rs. 150000"
-}]
+    "location": "Kochi",
+    "role": "Team Lead - Malayalam",
+    "experience": "Minimum 1 Years experience as TL in BPO",
+    "salary": "Up to 3 LPA"
+}, {
+    "id": 2,
+    "location": "Chennai",
+    "role": "Team Manager",
+    "experience": "Minim 6+ Years of experience in Banking Managing Team",
+    "salary": "14 to 15 LPA"
+},
+{
+    "id": 3,
+    "location": "TIdel Park Chennai",
+    "role": "Tele Sales - Kannada / Malayalam / Telugu / Tamil",
+    "experience": "Freshers",
+    "salary": "Up to 19k CTC"
+},
+{
+    "id": 4,
+    "location": "Ambattur Chennai",
+    "role": "Banking Officer - Kannada / Malayalam",
+    "experience": "Freshers",
+    "salary": "Up to 2LPA"
+},
+]
