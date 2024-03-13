ebca3705a6c383ce983074955ca2831bbef4e139
CareerSchoolHR <163095782+CareerSchoolHR@users.noreply.github.com>
2024-03-13 11:10:01 +0530

    13-03-2024 Jobs Update

--- src/data/JobsData.jsx
+++ src/data/JobsData.jsx
@@ -1,49 +1,147 @@
 export const jobs = [{
     "id": 1,
-    "location": "Kochi",
-    "role": "Team Lead - Malayalam",
-    "experience": "Minimum 1 Years experience as TL in BPO",
-    "salary": "Up to 3 LPA"
+    "location": "Ambattur Chennai",
+    "role": "Technical Support - Voice",
+    "experience": "Minimum 6 Months",
+    "salary": "Up to 4.5 LPA"
 }, {
     "id": 2,
-    "location": "Chennai",
-    "role": "Team Manager",
-    "experience": "Minim 6+ Years of experience in Banking Managing Team",
-    "salary": "14 to 15 LPA"
+    "location": "Ambattur Chennai",
+    "role": "Technical Support - Chat",
+    "experience": "Minimum 6 Months",
+    "salary": "Up to 3 LPA"
 },
 {
     "id": 3,
-    "location": "TIdel Park Chennai",
-    "role": "Tele Sales - Kannada / Malayalam / Telugu / Tamil",
-    "experience": "Freshers",
-    "salary": "Up to 19k CTC"
-},
-{
-    "id": 4,
-    "location": "Ambattur Chennai",
-    "role": "Banking Officer - Kannada / Malayalam",
-    "experience": "Freshers",
-    "salary": "Up to 2LPA"
-},
-{
-    "id": 5,
     "location": "Thoraipakkam",
     "role": "Voice Process Executive - Hindi / Malayalam",
     "experience": "Freshers",
     "salary": "Up to 4 LPA"
 },
 {
+    "id": 4,
+    "location": "Ambattur Chennai",
+    "role": "Accounts Payable",
+    "experience": "Minimum 1 Year",
+    "salary": "Up to 4 LPA"
+},
+{
+    "id": 5,
+    "location": "Perungudi Chennai",
+    "role": "Voice process - Telugu / Kannada",
+    "experience": "Freshers",
+    "salary": "Up to 16k CTC"
+},
+{
     "id": 6,
-    "location": "Mysuru",
-    "role": "Team Lead - BPO Kannada",
-    "experience": "Min 1 Year Exp as Team Lead",
-    "salary": "Up to 3 LPA"
+    "location": "Ambattur Chennai",
+    "role": "Banking Officer - Kannada / Malayalam",
+    "experience": "Freshers",
+    "salary": "Up to 2 LPA"
 },
 {
     "id": 7,
-    "location": "Mysuru",
-    "role": "QA - BPO Kannada",
-    "experience": "Min 1 Year Exp as Quality Analyst",
+    "location": "Tidel Park Chennai",
+    "role": "Tele Sales - Kannada / Malayalam / Telugu / Tamil",
+    "experience": "Freshers",
+    "salary": "Up to 19k CTC"
+},
+{
+    "id": 8,
+    "location": "Karapakkam Chennai",
+    "role": "Banking Customer Support - Hindi",
+    "experience": "Freshers / Experience",
     "salary": "Up to 3 LPA"
+},
+{
+    "id": 9,
+    "location": "Shivaji Nagar Bangalore",
+    "role": "Customer Support Executive",
+    "experience": "Freshers / Experience",
+    "salary": "Up to 3 LPA"
+},
+{
+    "id": 10,
+    "location": "Ambattur Chennai",
+    "role": "Hindi Voice Process",
+    "experience": "Freshers",
+    "salary": "Up to 25k CTC"
+},
+{
+    "id": 11,
+    "location": "Kochi",
+    "role": "Team Lead - Malayalam",
+    "experience": "Minimum 1 Year experience as TL in BPO",
+    "salary": "Up to 3 LPA"
+},
+{
+    "id": 12,
+    "location": "Chennai",
+    "role": "Team Manager",
+    "experience": "Minimum 6+ Years experience in Banking Managing Team",
+    "salary": "Up to 14 to 15 LPA"
+},
+{
+    "id": 13,
+    "location": "Chennai",
+    "role": "Banking customer support - Hindi",
+    "experience": "Minimum 2 Years experience in any Banking voice process",
+    "salary": "Up to 5 LPA"
+},
+{
+    "id": 14,
+    "location": "Chennai",
+    "role": "Admin Executive - Male",
+    "experience": "Minimum 1 Year experience as Office Admin",
+    "salary": "Upto 2.4 LPA"
+},
+{
+    "id": 15,
+    "location": "Mysuru",
+    "role": "Process Trainer - Kannada",
+    "experience": "Minimum 1 Year as Process Trainer",
+    "salary": "Upto 3.5 LPA"
+},
+{
+    "id": 16,
+    "location": "Chennai",
+    "role": "Process Associate Sinhala / German",
+    "experience": "Freshers",
+    "salary": "Up to 6 LPA"
+},
+{
+    "id": 17,
+    "location": "Chennai",
+    "role": "Quality Analyst",
+    "experience": "Minimum 1 Year Experience as QA in BPO",
+    "salary": "Upto 4 LPA"
+},
+{
+    "id": 18,
+    "location": "Mysuru",
+    "role": "Team Lead - Kannada",
+    "experience": "Minimum 1 Year experience as TL in BPO",
+    "salary": "Upto 3 LPA"
+},
+{
+    "id": 19,
+    "location": "Chennai",
+    "role": "HR - Employee Relations & Payroll",
+    "experience": "Minimum 2 Years experience as HR",
+    "salary": "Upto 4.5 LPA"
+},
+{
+    "id": 20,
+    "location": "Vijayawada",
+    "role": "Quality Analyst",
+    "experience": "Minimum 1 Year experience as Quality Analyst in BPO",
+    "salary": "Upto 3.5 LPA"
+},
+{
+    "id": 21,
+    "location": "Kochi",
+    "role": "Process Trainer - Malayalam",
+    "experience": "Minimum 1 Year experience as Process Trainer in BPO",
+    "salary": "Upto 3.5 LPA"
 },
 ]
