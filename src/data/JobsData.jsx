e87a19d59bca84504f39a97b3b92c36fd781f865
CareerSchoolHR <163095782+CareerSchoolHR@users.noreply.github.com>
2024-03-22 11:47:11 +0530

    22-03-2024 Jobs Update

--- src/data/JobsData.jsx
+++ src/data/JobsData.jsx
@@ -13,10 +13,10 @@ export const jobs = [{
 },
 {
     "id": 3,
-    "location": "Thoraipakkam",
-    "role": "Voice Process Executive - Hindi / Malayalam",
-    "experience": "Freshers",
-    "salary": "Up to 4 LPA"
+    "location": "Ambattur Chennai",
+    "role": "Semi Voice Process",
+    "experience": "Minimum 1 Year",
+    "salary": "Up to 2 LPA"
 },
 {
     "id": 4,
@@ -27,129 +27,129 @@ export const jobs = [{
 },
 {
     "id": 5,
+    "location": "Chennai",
+    "role": "Process Associate Sinhala / German",
+    "experience": "Freshers",
+    "salary": "Up to 10 LPA"
+},
+{
+    "id": 6,
     "location": "Perungudi Chennai",
     "role": "Voice process - Telugu / Kannada",
     "experience": "Freshers",
     "salary": "Up to 16k CTC"
 },
 {
-    "id": 6,
+    "id": 7,
     "location": "Ambattur Chennai",
     "role": "Banking Officer - Kannada / Malayalam",
     "experience": "Freshers",
     "salary": "Up to 2 LPA"
 },
 {
-    "id": 7,
+    "id": 8,
+    "location": "Saidapet",
+    "role": "Voice process -   Kannada/ Malayalam",
+    "experience": "Freshers",
+    "salary": "Up to 20k CTC"
+},
+{
+    "id": 9,
     "location": "Tidel Park Chennai",
     "role": "Tele Sales - Kannada / Malayalam / Telugu / Tamil",
     "experience": "Freshers",
     "salary": "Up to 19k CTC"
 },
 {
-    "id": 8,
+    "id": 10,
     "location": "Karapakkam Chennai",
     "role": "Banking Customer Support - Hindi",
     "experience": "Freshers / Experience",
     "salary": "Up to 3 LPA"
 },
 {
-    "id": 9,
+    "id": 11,
     "location": "Shivaji Nagar Bangalore",
     "role": "Customer Support Executive",
     "experience": "Freshers / Experience",
     "salary": "Up to 3 LPA"
 },
 {
-    "id": 10,
+    "id": 12,
     "location": "Ambattur Chennai",
     "role": "Hindi Voice Process",
     "experience": "Freshers",
     "salary": "Up to 25k CTC"
 },
 {
-    "id": 11,
+    "id": 13,
     "location": "Kochi",
     "role": "Team Lead - Malayalam",
     "experience": "Minimum 1 Year experience as TL in BPO",
     "salary": "Up to 3 LPA"
 },
 {
-    "id": 12,
+    "id": 14,
     "location": "Chennai",
     "role": "Team Manager",
     "experience": "Minimum 6+ Years experience in Banking Managing Team",
     "salary": "Up to 14 to 15 LPA"
 },
 {
-    "id": 13,
+    "id": 15,
     "location": "Chennai",
     "role": "Banking customer support - Hindi",
     "experience": "Minimum 2 Years experience in any Banking voice process",
     "salary": "Up to 5 LPA"
 },
 {
-    "id": 14,
+    "id": 16,
     "location": "Chennai",
     "role": "Admin Executive - Male",
     "experience": "Minimum 1 Year experience as Office Admin",
     "salary": "Upto 2.4 LPA"
 },
 {
-    "id": 15,
+    "id": 17,
     "location": "Mysuru",
     "role": "Process Trainer - Kannada",
     "experience": "Minimum 1 Year as Process Trainer",
     "salary": "Upto 3.5 LPA"
 },
 {
-    "id": 16,
-    "location": "Chennai",
-    "role": "Process Associate Sinhala / German",
-    "experience": "Freshers",
-    "salary": "Up to 6 LPA"
-},
-{
-    "id": 17,
+    "id": 18,
     "location": "Chennai",
     "role": "Quality Analyst",
     "experience": "Minimum 1 Year Experience as QA in BPO",
     "salary": "Upto 4 LPA"
 },
 {
-    "id": 18,
+    "id": 19,
     "location": "Mysuru",
     "role": "Team Lead - Kannada",
     "experience": "Minimum 1 Year experience as TL in BPO",
     "salary": "Upto 3 LPA"
 },
 {
-    "id": 19,
+    "id": 20,
     "location": "Chennai",
     "role": "HR - Employee Relations & Payroll",
     "experience": "Minimum 2 Years experience as HR",
     "salary": "Upto 4.5 LPA"
 },
 {
-    "id": 20,
+    "id": 21,
     "location": "Vijayawada",
     "role": "Quality Analyst",
     "experience": "Minimum 1 Year experience as Quality Analyst in BPO",
     "salary": "Upto 3.5 LPA"
 },
 {
-    "id": 21,
+    "id": 22,
     "location": "Kochi",
     "role": "Process Trainer - Malayalam",
     "experience": "Minimum 1 Year experience as Process Trainer in BPO",
     "salary": "Upto 3.5 LPA"
-},
-{
-    "id": 22,
-    "location": "Pan India",
-    "role": "Senior Power Apps Developer",
-    "experience": "7+ years",
-    "salary": "21 LPA"
 },
 {
     "id": 23,
