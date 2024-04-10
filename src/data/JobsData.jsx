b946361f533c314cea13a3e87a1671ef8d5a14c5
CareerSchoolHR <163095782+CareerSchoolHR@users.noreply.github.com>
2024-04-10 14:43:06 +0530

    Jobs Update 10/04/2024

--- src/data/JobsData.jsx
+++ src/data/JobsData.jsx
@@ -6,135 +6,128 @@ export const jobs = [{
     "salary": "Up to 4.5 LPA"
 }, {
     "id": 2,
-    "location": "Ambattur Chennai",
-    "role": "Technical Support - Chat",
-    "experience": "Minimum 6 Months",
+    "location": "Thoraipakkam",
+    "role": "Visa Verification Officer - Hindi",
+    "experience": "Freshers",
     "salary": "Up to 3 LPA"
 },
 {
     "id": 3,
-    "location": "Ekkathuthangal",
-    "role": "International Semi Voice Process",
-    "experience": "Freshers",
+    "location": "Ambattur Chennai",
+    "role": "Semi Voice Process",
+    "experience": "Minimum 1 year",
     "salary": "Up to 2 LPA"
 },
 {
     "id": 4,
-    "location": "Perungudi Chennai",
-    "role": "Voice process - Telugu / Kannada",
-    "experience": "Freshers",
-    "salary": "Up to 16k CTC"
-},
-{
-    "id": 5,
     "location": "Ambattur Chennai",
     "role": "Banking Officer - Kannada / Malayalam",
     "experience": "Freshers",
     "salary": "Up to 2 LPA"
 },
 {
-    "id": 6,
-    "location": "Ambattur Chennai",
-    "role": "Semi Voice Process",
-    "experience": "Minimum 1 Year",
-    "salary": "Up to 2 LPA"
+    "id": 5,
+    "location": "Thoraipakkam",
+    "role": "Visa Verification Officer - Malayalam",
+    "experience": "Freshers",
+    "salary": "Up to 3 LPA"
 },
 {
-    "id": 7,
+    "id": 6,
     "location": "Tidel Park Chennai",
     "role": "Tele Sales - Kannada / Malayalam / Telugu / Tamil",
     "experience": "Freshers",
     "salary": "Up to 19k CTC"
 },
 {
+    "id": 7,
+    "location": "Perungudi Chennai",
+    "role": "Voice process - Telugu / Kannada",
+    "experience": "Freshers",
+    "salary": "Up to 16k CTC"
+},
+{
     "id": 8,
+    "location": "Mount Road",
+    "role": "Relationship Officer",
+    "experience": "Freshers",
+    "salary": "Up to 25k CTC"
+},
+{
+    "id": 9,
     "location": "Shivaji Nagar Bangalore",
     "role": "Customer Support Executive",
     "experience": "Freshers / Experience",
     "salary": "Up to 3 LPA"
 },
 {
-    "id": 9,
+    "id": 10,
     "location": "Ambattur Chennai",
     "role": "Hindi Voice Process",
     "experience": "Freshers",
     "salary": "Up to 25k CTC"
 },
 {
-    "id": 10,
-    "location": "Kochi",
-    "role": "Team Lead - Malayalam",
-    "experience": "Minimum 1 Year experience as TL in BPO",
-    "salary": "Up to 3 LPA"
-},
-{
     "id": 11,
     "location": "Chennai",
-    "role": "Team Manager",
-    "experience": "Minimum 6+ Years experience in Banking Managing Team",
-    "salary": "Up to 14 to 15 LPA"
+    "role": "Operation Manager - Edu Tech",
+    "experience": "6+ Years in Edutech Industry handling operations",
+    "salary": "Up to 10 LPA"
 },
 {
     "id": 12,
     "location": "Chennai",
-    "role": "Banking customer support - Hindi",
-    "experience": "Minimum 2 Years experience in any Banking voice process",
-    "salary": "Up to 5 LPA"
+    "role": "Team Manager",
+    "experience": "6+ Years experience in Banking Managing Team",
+    "salary": "Up to 15 LPA"
 },
 {
     "id": 13,
     "location": "Chennai",
-    "role": "Admin Executive - Male",
-    "experience": "Minimum 1 Year experience as Office Admin",
-    "salary": "Upto 2.4 LPA"
+    "role": "Accounts Executive",
+    "experience": "Minimum 2 Years experience in Accounts",
+    "salary": "Up to 3 LPA"
 },
 {
     "id": 14,
-    "location": "Mysuru",
-    "role": "Process Trainer - Kannada",
-    "experience": "Minimum 1 Year as Process Trainer",
-    "salary": "Upto 3.5 LPA"
+    "location": "Chennai",
+    "role": "Admin Executive - Male",
+    "experience": "Minimum 1 Year experience as Office Admin",
+    "salary": "Up to 2.4 LPA"
 },
 {
     "id": 15,
     "location": "Chennai",
-    "role": "Quality Analyst",
-    "experience": "Minimum 1 Year Experience as QA in BPO",
-    "salary": "Upto 4 LPA"
+    "role": "Sales Executive",
+    "experience": "2 Years of Experience in any field or Telesales",
+    "salary": "Up to 3.6 LPA"
 },
 {
     "id": 16,
-    "location": "Mysuru",
-    "role": "Team Lead - Kannada",
-    "experience": "Minimum 1 Year experience as TL in BPO",
-    "salary": "Upto 3 LPA"
+    "location": "Chennai",
+    "role": "UI Automation - Testing",
+    "experience": "3+ Years of Experience in Testing",
+    "salary": "Up to 15 LPA"
 },
 {
     "id": 17,
     "location": "Chennai",
-    "role": "HR - Employee Relations & Payroll",
-    "experience": "Minimum 2 Years experience as HR",
-    "salary": "Upto 4.5 LPA"
+    "role": "WPF Programmer",
+    "experience": "Minimum 2 Years experience in Accounts",
+    "salary": "Up to 9 LPA"
 },
 {
     "id": 18,
-    "location": "Vijayawada",
-    "role": "Quality Analyst",
-    "experience": "Minimum 1 Year experience as Quality Analyst in BPO",
-    "salary": "Upto 3.5 LPA"
+    "location": "Chennai",
+    "role": "Jewelry Designer",
+    "experience": "3+ Years experience in Jewelry Designing",
+    "salary": "Up to 12 LPA"
 },
 {
     "id": 19,
-    "location": "Kochi",
-    "role": "Process Trainer - Malayalam",
-    "experience": "Minimum 1 Year experience as Process Trainer in BPO",
-    "salary": "Upto 3.5 LPA"
-},
-{
-    "id": 20,
-    "location": "Mount Road",
-    "role": "Relationship Officer",
-    "experience": "Freshers",
-    "salary": "Up to 25k CTC"
+    "location": "Chennai",
+    "role": "HR Generalist",
+    "experience": "Minimum 2 Years experience as HR Generalist",
+    "salary": "Up to 4 LPA"
 },
 ]
