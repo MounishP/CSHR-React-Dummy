9f540257325de7997e607980ca0601f89797fb9c
CareerSchoolHR <163095782+CareerSchoolHR@users.noreply.github.com>
2024-04-18 15:38:11 +0530

    Jobs Update 18/04/2024

--- src/data/JobsData.jsx
+++ src/data/JobsData.jsx
@@ -13,121 +13,163 @@ export const jobs = [{
 },
 {
     "id": 3,
+    "location": "Ekkathuthangal",
+    "role": "International Semi Voice Process",
+    "experience": "Freshers",
+    "salary": "Up to 2 LPA"
+},
+{
+    "id": 4,
     "location": "Ambattur Chennai",
     "role": "Semi Voice Process",
     "experience": "Minimum 1 year",
     "salary": "Up to 2 LPA"
 },
 {
-    "id": 4,
+    "id": 5,
+    "location": "Ambattur Chennai",
+    "role": "Business Advisory",
+    "experience": "Freshers",
+    "salary": "Up to 3 LPA"
+},
+{
+    "id": 6,
+    "location": "Aminjikarai",
+    "role": "Insurance Advisor",
+    "experience": "Freshers",
+    "salary": "Up to 2.5 LPA"
+},
+{
+    "id": 7,
     "location": "Ambattur Chennai",
     "role": "Banking Officer - Kannada / Malayalam",
     "experience": "Freshers",
     "salary": "Up to 2 LPA"
 },
 {
-    "id": 5,
+    "id": 8,
     "location": "Thoraipakkam",
     "role": "Visa Verification Officer - Malayalam",
     "experience": "Freshers",
     "salary": "Up to 3 LPA"
 },
 {
-    "id": 6,
+    "id": 9,
     "location": "Tidel Park Chennai",
     "role": "Tele Sales - Kannada / Malayalam / Telugu / Tamil",
     "experience": "Freshers",
     "salary": "Up to 19k CTC"
 },
 {
-    "id": 7,
+    "id": 10,
     "location": "Perungudi Chennai",
     "role": "Voice process - Telugu / Kannada",
     "experience": "Freshers",
     "salary": "Up to 16k CTC"
 },
 {
-    "id": 8,
+    "id": 11,
     "location": "Mount Road",
     "role": "Relationship Officer",
     "experience": "Freshers",
     "salary": "Up to 25k CTC"
 },
 {
-    "id": 9,
+    "id": 12,
+    "location": "Vadapalani",
+    "role": "Sales Officer / Loan Officer",
+    "experience": "Freshers",
+    "salary": "Up to 2.5 LPA"
+},
+{
+    "id": 13,
     "location": "Shivaji Nagar Bangalore",
     "role": "Customer Support Executive",
     "experience": "Freshers / Experience",
     "salary": "Up to 3 LPA"
 },
 {
-    "id": 10,
+    "id": 14,
     "location": "Ambattur Chennai",
     "role": "Hindi Voice Process",
     "experience": "Freshers",
     "salary": "Up to 25k CTC"
 },
 {
-    "id": 11,
+    "id": 15,
+    "location": "Karapakkam",
+    "role": "KYC Officer - Hindi",
+    "experience": "Freshers",
+    "salary": "Up to 25k CTC"
+},
+{
+    "id": 16,
     "location": "Chennai",
     "role": "Operation Manager - Edu Tech",
     "experience": "6+ Years in Edutech Industry handling operations",
     "salary": "Up to 10 LPA"
 },
 {
-    "id": 12,
+    "id": 17,
     "location": "Chennai",
     "role": "Team Manager",
     "experience": "6+ Years experience in Banking Managing Team",
     "salary": "Up to 15 LPA"
 },
 {
-    "id": 13,
+    "id": 18,
     "location": "Chennai",
     "role": "Accounts Executive",
     "experience": "Minimum 2 Years experience in Accounts",
     "salary": "Up to 3 LPA"
 },
 {
-    "id": 14,
+    "id": 19,
     "location": "Chennai",
     "role": "Admin Executive - Male",
     "experience": "Minimum 1 Year experience as Office Admin",
     "salary": "Up to 2.4 LPA"
 },
 {
-    "id": 15,
+    "id": 20,
     "location": "Chennai",
     "role": "Sales Executive",
     "experience": "2 Years of Experience in any field or Telesales",
     "salary": "Up to 3.6 LPA"
 },
 {
-    "id": 16,
+    "id": 21,
     "location": "Chennai",
     "role": "UI Automation - Testing",
     "experience": "3+ Years of Experience in Testing",
     "salary": "Up to 15 LPA"
 },
 {
-    "id": 17,
+    "id": 22,
     "location": "Chennai",
-    "role": "WPF Programmer",
-    "experience": "Minimum 2 Years experience in Accounts",
-    "salary": "Up to 9 LPA"
+    "role": "Social Media Analyst",
+    "experience": "Minimum 2 Years experience in WPF",
+    "salary": "Up to 5 LPA"
 },
 {
-    "id": 18,
-    "location": "Chennai",
-    "role": "Jewelry Designer",
-    "experience": "3+ Years experience in Jewelry Designing",
-    "salary": "Up to 12 LPA"
+    "id": 23,
+    "location": "Hyderabad",
+    "role": "Deputy Regional Manager",
+    "experience": "10+ Years of Experience in Banking",
+    "salary": "Up to 20 LPA"
 },
 {
-    "id": 19,
+    "id": 24,
     "location": "Chennai",
     "role": "HR Generalist",
     "experience": "Minimum 2 Years experience as HR Generalist",
     "salary": "Up to 4 LPA"
+},
+{
+    "id": 25,
+    "location": "Chennai",
+    "role": "Front End Developer",
+    "experience": "Minimum 5 Years in HTML, Javascript, etc",
+    "salary": "Up to 15 LPA"
 },
 ]
